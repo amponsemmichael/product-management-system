c37a79670e213bd14b334f44060ab9085d579031
amponsemmichael <amponsem_michael@yahoo.com>
2024-07-13 22:55:07 +0000

    committing changes to backend after successful crud operations

--- product-management/src/main/java/com/amponsem/product_management/controller/ProductController.java
+++ product-management/src/main/java/com/amponsem/product_management/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.amponsem.product_management.model.Product;
 import com.amponsem.product_management.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,34 +20,43 @@ public class ProductController {
     @PostMapping
     public ResponseEntity<String> createProduct(@RequestBody Product product){
         productService.addProduct(product);
-        return ResponseEntity.ok().body("Product created successfully");
+        return new  ResponseEntity<>("Product created successfully", HttpStatus.CREATED);
     }
 
     @GetMapping("/name/{name}")
-    public List<Product> getProductsByName(@PathVariable String name){
-        return productService.findByName(name);
+    public ResponseEntity<List<Product>> getProductsByName(@PathVariable String name){
+        List<Product> products = productService.findByName(name);
+        return new ResponseEntity<>(products,HttpStatus.OK);
     }
 
     @GetMapping("/price")
-    public List<Product> getProductsByPriceRange(@RequestParam Double minPrice, @RequestParam Double maxPrice){
-        return productService.findByPriceBetween(minPrice, maxPrice);
+    public ResponseEntity<List<Product>> getProductsByPrice(){
+        Double minPrice = 0.0;
+        Double maxPrice = 0.0;
+        List<Product> products = productService.findByPriceBetween(minPrice,maxPrice);
+     return new ResponseEntity<>(products,HttpStatus.OK);
     }
 
     @GetMapping("/category/{category}")
-    public List<Product> getProductsByCategory(@PathVariable String category){
-        return productService.findByCategory(category);
+    public ResponseEntity<List<Product>> getProductsByCategory(@PathVariable String category){
+        List<Product> products = productService.findByCategory(category);
+        return new ResponseEntity<>(products,HttpStatus.OK);
     }
 
     @GetMapping
-    public Page<Product> getAllProducts(@RequestParam int page, @RequestParam int size, @RequestParam String sortBy){
-        return productService.findAll(page, size,sortBy);
+    public ResponseEntity<Page<Product>> getAllProducts(
+        @RequestParam(required = false, defaultValue = "0") int page,
+        @RequestParam(required = false, defaultValue = "10") int size,
+        @RequestParam(required = false, defaultValue = "name") String sortBy){
+                Page<Product> products = productService.findAll(page,size,sortBy);
+         return new ResponseEntity<>(products,HttpStatus.OK);
     }
 
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteProduct(@PathVariable String id){
         productService.deleteProduct(id);
-        return ResponseEntity.ok().body("Product deleted successfully");
+        return new ResponseEntity<>("Product deleted successfully", HttpStatus.OK);
     }
 
 }
--- product-management/src/main/java/com/amponsem/product_management/model/Product.java
+++ product-management/src/main/java/com/amponsem/product_management/model/Product.java
@@ -1,9 +1,13 @@
 package com.amponsem.product_management.model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Setter
+@Getter
 @Document(collection = "products")
 @Data
 public class Product {
@@ -15,51 +19,4 @@ public class Product {
     private String category;
     private Integer quantity;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
 }
